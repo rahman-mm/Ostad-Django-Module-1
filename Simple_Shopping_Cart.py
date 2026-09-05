cbd13c2dc9962e9e7d7dec9ae75e80f6d7a2e0a6
Md. Mahfujur Rahman <rahman.mm016@gmail.com>
2026-09-05 20:20:16 +0600

    Add Module-1 Assignments

---
+++ Simple_Shopping_Cart.py
@@ -0,0 +1,47 @@
+name= input("Give me the customer name: ")
+
+product_1= input("First product name: ")
+product_1_price= float(input("Price of first product: "))
+
+product_2= input("Second product name: ")
+product_2_price= float(input("Price of second product: "))
+
+product_3= input("Third product name: ")
+product_3_price= float(input("Price of third product: "))
+
+
+subtotal= product_1_price + product_2_price + product_3_price
+
+if subtotal >= 5000:
+    discount= subtotal*(20/100)
+
+elif 3000 <= subtotal <= 4999:
+    discount= subtotal*(10/100)
+
+elif 1000 <= subtotal <= 2999:
+    discount= subtotal*(5/100)
+
+else:
+    discount= 0
+
+
+total= subtotal-discount
+
+print(f"Customer Name: {name}")
+print()
+
+print(f"Product 1: {product_1}")
+print(f"Price: {product_1_price}")
+print()
+
+print(f"Product 2: {product_2}")
+print(f"Price: {product_2_price}")
+print()
+
+print(f"Product 2: {product_3}")
+print(f"Price: {product_3_price}")
+print()
+
+print(f"Subtotal: {subtotal}")
+print(f"Discount: {discount}")
+print(f"Final Total: {total}")
---
+++ Student_Grade_Calculator.py
@@ -0,0 +1,34 @@
+name= input("Give me the student name: ")
+
+sub_1_marks= float(input("Input the marks of first subject:"))
+sub_2_marks= float(input("Input the marks of second subject:"))
+sub_3_marks= float(input("Input the marks of third subject:"))
+
+print(f"Student Name: {name}")
+
+total_marks= sub_1_marks + sub_2_marks + sub_3_marks
+print(f"Total Marks: {total_marks}")
+
+average_marks= round(total_marks/3, 2)
+print(f"Average: {average_marks}")
+
+
+if average_marks >= 80:
+    grade= "A+"
+    print(f"Grade: {grade}")
+
+elif 70 <= average_marks <=79:
+    grade= "A"
+    print(f"Grade: {grade}")
+
+elif 60 <= average_marks <=69:
+    grade= "B"
+    print(f"Grade: {grade}")
+
+elif 50 <= average_marks <=59:
+    grade= "C"
+    print(f"Grade: {grade}")
+
+else:
+    grade= "F"
+    print(f"Grade: {grade}")
